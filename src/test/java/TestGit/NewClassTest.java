b48c61b183ab65884005050f5bac37d735287bfc
Neha Yadav <nehayadav710.ny@gmail.com>
2025-08-23 09:01:33 +0530

    test

--- src/test/java/TestGit/NewClassTest.java
+++ src/test/java/TestGit/NewClassTest.java
@@ -4,6 +4,10 @@ public class NewClassTest {
 	
 	public static void main(String[] args) {
 		System.out.println("check conflict");
+		System.out.println("check conflict");
+		System.out.println("check conflict");
+		System.out.println("check conflict");
+		System.out.println("check conflict");
 	}
 
 }
