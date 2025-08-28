5350b3a0e0cd500d74b2605e258887b911c885dc
Neha Yadav <nehayadav710.ny@gmail.com>
2025-08-28 09:06:50 +0530

    Test case in student file

--- src/test/java/TestGit/Student.java
+++ src/test/java/TestGit/Student.java
@@ -1,9 +1,17 @@
 package TestGit;
 
+import org.testng.annotations.Test;
+
 public class Student {
 	
 	public static void main(String[] args) {
 		System.out.println("student clas is created");
 	}
+	
+	@Test
+	public void testABC() {
+		System.out.println("Neha is working on tc 2");
+	}
+	
 
 }
