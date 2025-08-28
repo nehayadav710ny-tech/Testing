6f4cd49a5c023cfdb5bfaad8e6b4b77085afbf9d
Neha Yadav <nehayadav710.ny@gmail.com>
2025-08-28 09:34:53 +0530

    new test campaign

---
+++ src/test/java/TestGit/Campaign.java
@@ -0,0 +1,18 @@
+package TestGit;
+
+import org.testng.annotations.Test;
+
+public class Campaign {
+	
+	@Test
+	public void testABCwww() {
+		System.out.println("Neha is working on tc 2");
+	}
+	
+
+	@Test
+	public void testCapaignwww() {
+		System.out.println(" Virat kohli is working on tc 4,5");
+	}
+
+}
